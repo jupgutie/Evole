abf7496737c44a679b0e8c246c23862f7ce16a57
slimcoz <brayan102011@gmail.com>
2024-10-07 12:50:59 -0500

    pagina terminada parte alejandro

--- js/scriptC.js
+++ js/scriptC.js
@@ -12,3 +12,22 @@ function toggleProductos(tituloCategoria) {
     // Alterna la visibilidad de la lista seleccionada
     listaProductos.style.display = (listaProductos.style.display === 'none' || listaProductos.style.display === '') ? 'block' : 'none';
 }
+
+// Capturamos el clic en el icono de usuario
+document.getElementById("user-icon").addEventListener("click", function() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    // Alternamos entre mostrar y ocultar el menú
+    if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+        dropdownMenu.style.display = "block";
+    } else {
+        dropdownMenu.style.display = "none";
+    }
+});
+
+// Cerrar el menú si se hace clic fuera de él
+window.addEventListener("click", function(event) {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    if (event.target !== document.getElementById("user-icon")) {
+        dropdownMenu.style.display = "none";
+    }
+});
